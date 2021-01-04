6161366ce5eacc00e7ca94a36a7e4276acc429c3
viranca <37591728+viranca@users.noreply.github.com>
2021-01-04 14:29:21 +0100

    Q2 update

--- Assignment_2/ass2_q2.py
+++ Assignment_2/ass2_q2.py
@@ -4,7 +4,6 @@ import numpy as np
 
 
 #part B
-
 #define input matrices A, B, C and V:
 A = np.array([[1, 1],   
               [0, 1]])
@@ -17,7 +16,7 @@ C = np.array([1, 0])
 V = np.array([0, 0])
 
 #number of monte carlo iterations:
-MC_n = 1000
+MC_n = 1
 t_list = []
 for MC_i in range(MC_n):
     X_t = np.array([[0, 0], [0, 0]]) 
@@ -31,11 +30,11 @@ for MC_i in range(MC_n):
         V[1] = np.random.normal(0, 1)      
         
         #compute X_t+1 at every timestep t
-        X_tplusone = A * X_t + B + V
-        
+        X_tplusone = np.dot(A, X_t) + B + V
+        print(X_tplusone)
         #compute Y_t at every timestep t        
-        Y_t = C * X_t + e   
-        Y_t = Y_t[0][0]
+        Y_t = np.dot(C, X_t) + e   
+        #Y_t = Y_t[0][0]
         
         #Move on to the next timestep t
         X_t = X_tplusone
@@ -58,17 +57,17 @@ X_3_2 = []
 X_3_3 = []
 X_3_4 = []
 
-for i in range(100000):
+for i in range(1):
     X_t = np.array([0, 0]) 
     
     #add the timestep here:
-    for t in range(5):
+    for t in range(4):
         #draw new random variables at every timestep t!
         V[0] = np.random.normal(0, 1)
         V[1] = np.random.normal(0, 1) 
         
         #compute X_t+1 at every timestep t
-        X_tplusone = A * X_t + B + V
+        X_tplusone = np.dot(A, X_t) + B + V
         
         #Move on to the next timestep t
         X_t = X_tplusone
@@ -84,7 +83,10 @@ print(statistics.mean(X_3_3))
 print(statistics.mean(X_3_4))
 #%%
 #trying the analytical solution for E[X_3]::
-print(A*A *B + A * B + B)
+
+print(np.dot(np.dot(A, A), B) + np.dot(A, B) + B)
+
+print(A*A*B + A*B + B)
 
 #trying analytical solution for E[X_2]:
 print(A * B + B)
